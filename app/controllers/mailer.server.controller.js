3df437116c5f39ade7ef6b7206b3dd50638e23e4
Asche <chris.asche@gmail.com>
2015-05-15 17:12:29 -0300

    works

--- app/controllers/mailer.server.controller.js
+++ app/controllers/mailer.server.controller.js
@@ -4,16 +4,18 @@ var api_key = 'key-55d354fbcfb543701253c20dce76c0b9';
 var domain = 'app552b0f23610b46bbaa83fc1579d92b51.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 var subscriberlist = 'subscribers@app552b0f23610b46bbaa83fc1579d92b51.mailgun.org';
+var ejs = require('ejs')
+  ,fs = require('fs')
+  ,str = fs.readFileSync(__dirname + '/../templates/newjob.server.template.ejs', 'utf8');
 
-exports.emailJobs = function() {
+exports.emailJobs = function(jobs, quit) {
   var data = {
-    from: 'Excited User <me@samples.mailgun.org>',
-    to: 'serobnic@mail.ru',
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
+    from: 'Gitajob <subscribers@app552b0f23610b46bbaa83fc1579d92b51.mailgun.org>',
+    to: 'subscribers@app552b0f23610b46bbaa83fc1579d92b51.mailgun.org',
+    subject: 'There are ' + jobs.length + ' new jobs at Github',
+    html: ejs.render(str, { jobs: jobs })
   };
-
   mailgun.messages().send(data, function (error, body) {
-    console.log(body);
+    error ? quit(error) : quit();
   });
 };

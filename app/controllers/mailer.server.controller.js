948df2267be78bb9502aea84318f31f2ddc81600
Asche <chris.asche@gmail.com>
2015-05-15 17:50:08 -0300

    test

--- app/controllers/mailer.server.controller.js
+++ app/controllers/mailer.server.controller.js
@@ -4,9 +4,10 @@ var api_key = 'key-55d354fbcfb543701253c20dce76c0b9';
 var domain = 'app552b0f23610b46bbaa83fc1579d92b51.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 var subscriberlist = 'subscribers@app552b0f23610b46bbaa83fc1579d92b51.mailgun.org';
-var ejs = require('ejs')
-  ,fs = require('fs')
-  ,str = fs.readFileSync('./app/templates/newjob.server.template.ejs', 'utf8');
+var ejs = require('ejs'),
+  fs = require('fs'),
+  path = require('path'),
+  str = fs.readFileSync(path.join(process.cwd(), 'app/templates/newjob.server.template.ejs'), 'utf8');
 
 exports.emailJobs = function(jobs, quit) {
   var data = {

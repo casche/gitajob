254d30f2d92bb45edab790acdb63341ba787a45a
Asche <chris.asche@gmail.com>
2015-11-04 16:23:31 -0400

    fixes a bug that caused a crash because the lcoation of config values changed

--- app/services/mailer.server.service.js
+++ app/services/mailer.server.service.js
@@ -5,14 +5,14 @@ var ejs = require('ejs');
 var fs = require('fs');
 var str = fs.readFileSync('./app/templates/newJob.server.template.ejs', 'utf8');
 var mailgun = require('mailgun-js')({
-  apiKey: config.mailer.apiKey,
-  domain: config.mailer.domain
+  apiKey: config.server.mailer.apiKey,
+  domain: config.server.mailer.domain
 });
 
 exports.emailJobs = function(jobs) {
   var data = {
-    from: config.mailer.from,
-    to: config.mailer.subscriberList,
+    from: config.server.mailer.from,
+    to: config.server.mailer.subscriberList,
     subject: 'There ' + (jobs.length > 1 ? 'are ' : 'is ') + jobs.length + ' new job' + (jobs.length > 1 ? 's' : '') +
       ' at Github',
     html: ejs.render(str, {
@@ -22,6 +22,6 @@ exports.emailJobs = function(jobs) {
   console.log('There ' + (jobs.length > 1 ? 'are ' : 'is ') + jobs.length + ' new job' + (jobs.length > 1 ? 's' : '') +
     ' at Github');
   console.log(config);
-  console.log('Sending mail to ' + config.mailer.subscriberList);
+  console.log('Sending mail to ' + config.server.mailer.subscriberList);
   mailgun.messages().send(data, function(error, body) {});
 };

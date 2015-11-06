e4fa0c0ae861d135144d051e0bd68faa4638ef52
Asche <chris.asche@gmail.com>
2015-11-06 13:59:37 -0400

    Clean up before and after each

--- tests/mailer.service.test.js
+++ tests/mailer.service.test.js
@@ -5,6 +5,16 @@ var mongoose = require('mongoose');
 var mailer = require(__dirname + '/../app/services/mailer.server.service.js');
 
 describe('Mailer service', function() {
+  var sandbox;
+    
+  beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+      sandbox.restore();
+  });
+
   describe('Should mail jobs if any exist', function() {
   });
 });

381c599ed6a4311ce724e12c3cf39af8a2f07d92
Asche <chris.asche@gmail.com>
2015-11-06 13:58:08 -0400

    adds test file for mailer service

---
+++ tests/mailer.service.test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+chai.should();
+var sinon = require('sinon');
+var mongoose = require('mongoose');
+var mailer = require(__dirname + '/../app/services/mailer.server.service.js');
+
+describe('Mailer service', function() {
+  describe('Should mail jobs if any exist', function() {
+  });
+});

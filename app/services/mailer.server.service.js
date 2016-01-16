82ebf4fa0c98ef339728a6954f82b066fef86e1d
Asche <chris.asche@gmail.com>
2016-01-16 15:57:54 -0500

    Refactors mailer.server.service

--- app/services/mailer.server.service.js
+++ app/services/mailer.server.service.js
@@ -9,21 +9,29 @@ var mailgun = require('mailgun-js')({
   domain: config.server.mailer.domain
 });
 
+function hasJobs(jobs) {
+  return jobs && jobs.length > 0;
+}
+
+function mailJobs(jobs, subject) {
+  console.log('Sending mail to ' + config.server.mailer.subscriberList);
+  mailgun.messages().send({
+    from: config.server.mailer.from,
+    to: config.server.mailer.subscriberList,
+    subject: subject,
+    html: ejs.render(str, {
+      jobs: jobs
+    })
+  }, function(error, body) {
+    console.log('Could not send jobs ' + error);
+  });
+}
+
 exports.emailJobs = function(jobs) {
-  if (jobs && jobs.length > 0) {
-    var data = {
-      from: config.server.mailer.from,
-      to: config.server.mailer.subscriberList,
-      subject: 'There ' + (jobs.length > 1 ? 'are ' : 'is ') + jobs.length + ' new job' + (jobs.length > 1 ? 's' : '') +
-        ' at Github',
-      html: ejs.render(str, {
-        jobs: jobs
-      })
-    };
-    console.log('There ' + (jobs.length > 1 ? 'are ' : 'is ') + jobs.length + ' new job' + (jobs.length > 1 ? 's' : '') +
-      ' at Github');
-    console.log('Sending mail to ' + config.server.mailer.subscriberList);
-    mailgun.messages().send(data, function(error, body) {});
+  if (hasJobs(jobs)) {
+    mailJobs(jobs, 'There ' + (jobs.length > 1 ? 'are ' : 'is ') + jobs.length +
+      ' new job' + (jobs.length > 1 ? 's' : '') +
+      ' at Github')
   } else {
     console.log('No jobs :(');
   }

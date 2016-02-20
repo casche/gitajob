1db4448b0505e49ffefaf0aa1bd2e67daab5a19d
Asche <chris.asche@gmail.com>
2016-02-20 11:54:34 -0400

    Adds back the missing create-a-scrape functionality that i've left out for EONS :smile:

--- bin/github/github.jobs.js
+++ bin/github/github.jobs.js
@@ -2,12 +2,34 @@ var GitHubParse = require('./parse.service.js');
 var GitHubJobSite = require('./github.jobsite.js');
 var Job = require('./jobs.github.dao.js');
 var Q = require('q');
+var Scrapes = require('../../app/services/scrapes.server.service.js');
+var C = (require('../../config/env/' + (process.env.NODE_ENV || 'development') + '.js')).server;
 
 module.exports = (function() {
   return {
-    getJobs: function(res) {
+    getJobUrls: function(res) {
+      var d = Q.defer();
+      d.resolve(GitHubParse.getJobUrls(res.html));
+
+      return d.promise;
+    },
+
+    createScrape: function(urls) {
+        var d = Q.defer();
+        Scrapes.create(C.jobUrl, urls.length, function(err, scrape) {
+            if (err) {
+                d.reject(urls, err);
+            } else {
+                d.resolve(urls);
+            }
+        });
+
+        return d.promise;
+    },
+
+    getJobs: function(urls) {
       var promises = [];
-      GitHubParse.getJobUrls(res.html).forEach(function(url) {
+      urls.forEach(function(url) {
         promises.push(GitHubJobSite.html(url));
       });
 
